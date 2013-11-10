116b79a14858255dc72b446c32f32548102cf80b
Cory Borg <cborg@oanda.com>
2013-11-10 12:33:50 -0500

    Adds combo select box for locations.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,39 @@
 'use strict';
 
-var panda = angular.module('pandamoniumApp');
+var app = angular.module('pandamoniumApp');
 
-panda.controller('MainCtrl', function ($scope) {
-
+app.controller('MainCtrl', function ($scope) {
+  // Built-in support for ajax
+   $scope.locationSelect = {
+     minimumInputLength: 1,
+     ajax: {
+       url: "/api/v1/venues/explore/43.6460675/-79.39230429999999",
+       data: function (term, page) {
+         return {};
+       },
+       results: function (data, page) {
+         console.log(JSON.parse(data).response.groups[0].items)
+         return {"results": JSON.parse(data).response.groups[0].items};
+       }
+     },
+     formatResult: locationFormatResult,
+     formatSelection: locationFormatSelection
+   }
 });
+
+function locationFormatResult(location) {
+    return "<li class='venue-info'>" + location.venue.name + "</li>";;
+}
+
+function locationHash(location) {
+    var hashed = { 
+      "name": location.venue.name,
+      "lat": location.venue.location ? location.venue.location.lat : '',
+      "lng": location.venue.location ? location.venue.location.lng : ''
+    }
+    return JSON.stringify(hashed);
+}
+
+function locationFormatSelection(location) {
+  return location.venue.name;
+}
