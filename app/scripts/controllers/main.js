72d978f0ebd0c284703facb759a632d6d62a8d8d
Mark Jubenville <ioncache@gmail.com>
2013-11-10 16:35:30 -0500

    fixed up the way the home page updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,8 +6,36 @@ app.controller('MainCtrl', function ($scope, $sanitize, $resource, $rootScope, $
     $rootScope.controller = 'MainCtrl';
 
     var Question = $resource('/api/v1/question/:id', { id: '@id' });
+    $scope.questions = Question.query();
+
     var recentQuestionInterval = setInterval( function() {
-        $scope.questions = Question.query();
+        var newQuestions = Question.query();
+        var currentLength = $scope.questions.length;
+        var currentLastId = $scope.questions[currentLength-1].id;
+        var foundAtIndex;
+        var currentIds = _.pluck($scope.questions, 'id');
+        var newIds     = _.pluck(newQuestions, 'id');
+
+        var differentQuestions = _.filter(newQuestions, function(item) {
+            return !_.contains(currentIds, item.id);
+        });
+
+        if ( differentQuestions.length > 0 ) {
+            var numToRemove = differentQuestions.length + currentLength - 20;
+
+            if ( numToRemove <= 0 ) {
+                for ( var i = 0 ; i < differentQuestions.length ; i++ ) {
+                    $scope.questions.push(differentQuestions.shift());
+                }
+            }
+            else {
+                for ( var i = 0 ; i < numToRemove ; i++ ) {
+                    $scope.questions.shift();
+                    $scope.questions.push(differentQuestions.shift());
+                }
+            }
+        }
+
     }, 1000);
 
     $scope.locationSelect = {
