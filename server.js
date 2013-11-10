a4677c237bb3ef43c9d10357339587c173039fdd
Mark Jubenville <ioncache@gmail.com>
2013-11-09 21:00:42 -0500

    some router changes

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ var async          = require('async'),
     http           = require('http'),
     qs             = require('querystring'),
     rest           = require('restler'),
+    send           = require('send'),
     socketio       = require('socket.io'),
-    staticContent  = require('send');
+    url            = require('url');
 
 // our modules
 var question       = require('./backend/question');
@@ -16,37 +17,60 @@ var question       = require('./backend/question');
 var isProduction = (process.env.NODE_ENV === 'production');
 var port = (isProduction ? 80 : 8000);
 
-function staticContent() {
-  staticContent(this.req, this.req.url).root('dist').pipe(response);
+var staticContent = function(req, res) {
+    //console.log(url.parse(req.url));
+
+    var staticError = function(err) {
+        res.statusCode = err.status || 500;
+        res.end(err.message);
+    }
+    
+    var staticRedirect = function() {
+        res.statusCode = 301;
+        res.setHeader('Location', req.url + '/');
+        res.end('Redirecting to ' + req.url + '/');
+    }
+
+    send(req, url.parse(req.url).pathname)
+    .root('dist')
+    .on('error', staticError)
+    .on('directory', staticRedirect)
+    .pipe(res);
 }
 
 var router = new director.http.Router({
-  '/api/v1': {
-    '/question': {
-      '/(\\w+)': {
-        get: function (id) { question.get(this.res, id); }
-      },
-      get: question.list,
-      put: question.add
+    '/api/v1': {
+        '/question': {
+            '/(\\w+)': {
+                get: function (id) { question.get(this.res, id); }
+            },
+            get: question.list,
+            put: question.add
+        }
+    },
+    '/.*': {
+        get: function() {
+            console.log(this.req.url);
+            staticContent(this.req, this.res);
+        }
     }
-  },
-  get: staticContent
 });
 
-
 function onRequest(request, response) {
-  router.dispatch(request, response, function (err) {
-    if (err) {
-      response.writeHead(404);
-      response.end();
-    }
-  });
+    router.dispatch(request, response, function (err) {
+        console.log(request.url);
+        if (err) {
+            console.log(err);
+            response.writeHead(err.status, { 'Content-Type': 'text/plain' });
+            response.end(err.message);
+        }
+    });
 }
 
 function serverError(err) {
-  if (err) { console.error(err); process.exit(-1); }
+    if (err) { console.error(err); process.exit(-1); }
 
-  console.log('Server running at http://0.0.0.0:' + port + '/');
+    console.log('Server running at http://0.0.0.0:' + port + '/');
 }
 
 http.createServer(onRequest).listen(port, serverError);
