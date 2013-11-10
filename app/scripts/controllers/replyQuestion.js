6e22d3909ca68ea8bdefd6144e4b74b5fab1188c
Mark Jubenville <ioncache@gmail.com>
2013-11-10 17:30:15 -0500

    made all posters anonymous

--- app/scripts/controllers/replyQuestion.js
+++ app/scripts/controllers/replyQuestion.js
@@ -2,6 +2,9 @@
 
 var app = angular.module('pandamoniumApp');
 
-app.controller('ReplyQuestionCtrl', function ($scope, $rootScope) {
+app.controller('ReplyQuestionCtrl', function ($scope, $rootScope, $resource, $routeParams) {
     $rootScope.controller = 'ReplyQuestionCtrl';
+
+    var Question = $resource('/api/v1/question/:id', { id: '@id' });
+    $scope.question = Question.get({ id: $routeParams.id });
 });
